6303f7822b8f5bc2467edbbff02b9d464b37af38
rkburnside <richard@burnsides.org>
2014-03-17 22:28:52 -0600

    the Cardboard_Robot.ino is working, updated, and good to go.
    Signed-off-by: rkburnside <richard@burnsides.org>

--- Cardboard_Robot/Cardboard_Robot.ino
+++ Cardboard_Robot/Cardboard_Robot.ino
@@ -10,42 +10,70 @@ AF_Stepper motor2(200, 2);
 //servo declaration
 Servo servo1;
 
-void setup()
-{
+void setup(){
 	pinMode(A5, OUTPUT);      // sets the digital pin as output
-
 	digitalWrite(A5, HIGH);   // sets the LED on
 
 	servo1.attach(9);
-	motor1.setSpeed(10);  // 10 rpm   
-	motor1.step(5, FORWARD, SINGLE); 
-
-	motor2.setSpeed(10);  // 10 rpm   
-	motor2.step(15, FORWARD, SINGLE); 
+	motor1.setSpeed(10);  // 10 rpm
+	motor2.setSpeed(10);  // 10 rpm
 
 	motor1.release();
 	motor2.release();
-	delay(1000);
+	
+	delay(2500);
 }
 
 void loop()
 {
-	static int time = 0;
+	static double time = 0;
+
+	while((millis() - time) < 5000){
+		motor1.step(1, FORWARD, INTERLEAVE);
+		motor2.step(1, FORWARD, INTERLEAVE);
+		servo1.write(30);
+	}
 	
-	motor1.step(1, FORWARD, SINGLE); 
-	motor2.step(1, BACKWARD, SINGLE); 
 	motor1.release();
 	motor2.release();
 	
-	if((millis() - time) > 2500){
-		motor1.release();
-		motor2.release();
+	blink_led();
+
+	time = millis();
+
+	while((millis() - time) < 5000){
+		motor1.step(1, BACKWARD, INTERLEAVE);
+		motor2.step(1, BACKWARD, INTERLEAVE);
 		servo1.write(150);
-		delay(10000);
-		time = millis();
 	}
-	else{
-		servo1.write(30);
+	
+	motor1.release();
+	motor2.release();
+
+	blink_led();
+	
+	time = millis();
+
+	if(millis() > 300000){
+			servo1.detach();
+			motor1.release();
+			motor2.release();
+			digitalWrite(A5, LOW);   // sets the LED on
+		while(1){}
+	}
+	
+}
+
+void blink_led(){
+	servo1.detach();
+
+	for(int i = 0; i<40; i++){
+		digitalWrite(A5, !digitalRead(A5));   // sets the LED on
+		delay(250);
 	}
 
+	digitalWrite(A5, HIGH);   // sets the LED on
+
+	servo1.attach(9);
+	return;
 }
